6268c30f49ee96f156891b4003c5fbeb44dad880
hfhsh <linexy@naver.com>
2023-11-10 17:59:36 +0900

    Feat: 임시저장

--- lib/my_swipe_to_dissmiss.dart
+++ lib/my_swipe_to_dissmiss.dart
@@ -8,13 +8,32 @@ class MySwipeToDismiss extends StatefulWidget {
 }
 
 class _MySwipeToDismissState extends State<MySwipeToDismiss> {
+  final items = List<String>.generate(20, (index) => "Item ${index + 1}");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Swipe To Dismiss'),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return Dismissible(
+            background: Container(color: Colors.red,),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) {
+              setState(() {
+                items.remove(index);
+              });
+            },
+            child: ListTile(
+              title: Text('${items[index]}'),
+            ),
+            key: Key(item),
+          );
+        },
+      ),
     );
   }
 }
